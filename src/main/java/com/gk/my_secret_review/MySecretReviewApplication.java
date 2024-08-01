3ac61def48aa40ff58001e1f17a32484978dfdd9
gk <iba3374@naver.com>
2024-08-01 11:06:02 +0900

    first commit

---
+++ src/main/java/com/gk/my_secret_review/MySecretReviewApplication.java
@@ -0,0 +1,13 @@
+package com.gk.my_secret_review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySecretReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySecretReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gk/my_secret_review/MySecretReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gk.my_secret_review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySecretReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
