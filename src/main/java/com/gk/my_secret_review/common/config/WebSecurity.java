4a1770cb25927835c5062e9cb287e8ddf8d28a95
gk <iba3374@naver.com>
2024-08-02 00:13:37 +0900

    feat: 인가 필터 추가

--- src/main/java/com/gk/my_secret_review/common/config/WebSecurity.java
+++ src/main/java/com/gk/my_secret_review/common/config/WebSecurity.java
@@ -1,5 +1,6 @@
 package com.gk.my_secret_review.common.config;
 
+import com.gk.my_secret_review.common.filter.AuthorizationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -7,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -16,8 +18,8 @@ public class WebSecurity {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .headers(h -> h.frameOptions(f -> f.disable()));
-
+                .headers(h -> h.frameOptions(f -> f.disable()))
+                .addFilterBefore(new AuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
