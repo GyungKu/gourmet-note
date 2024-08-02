54e7421193b9b789cad4482e84ed459c1cbe27a6
gk <iba3374@naver.com>
2024-08-02 19:52:27 +0900

    feat: 가게 검색 로그인한 사용자만 가능하게 설정

--- src/main/java/com/gk/my_secret_review/shop/controller/ShopController.java
+++ src/main/java/com/gk/my_secret_review/shop/controller/ShopController.java
@@ -4,13 +4,15 @@ import com.gk.my_secret_review.shop.service.ShopService;
 import com.gk.my_secret_review.shop.vo.ResponseShopList;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/shop")
+@RequestMapping("/api/v1/shops")
+@PreAuthorize("isAuthenticated()")
 public class ShopController {
 
     private final ShopService service;
