6f57eb1c3e3969c814ba99a35b2e99940c53c493
gk <iba3374@naver.com>
2024-08-02 19:54:26 +0900

    feat: 로그인 안 한 회원을 403으로 튕겨내서 엔트리포인트 커스텀 설정

--- src/main/java/com/gk/my_secret_review/common/config/WebSecurity.java
+++ src/main/java/com/gk/my_secret_review/common/config/WebSecurity.java
@@ -1,6 +1,7 @@
 package com.gk.my_secret_review.common.config;
 
 import com.gk.my_secret_review.common.filter.AuthorizationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -13,12 +14,16 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class WebSecurity {
+
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .headers(h -> h.frameOptions(f -> f.disable()))
+                .exceptionHandling(e -> e.authenticationEntryPoint(customAuthenticationEntryPoint))
                 .addFilterBefore(new AuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
