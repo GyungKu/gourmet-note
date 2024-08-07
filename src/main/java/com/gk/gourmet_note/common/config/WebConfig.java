82f280ad8ed66881b52ba5a1c154d1c8745fe20f
gk <iba3374@naver.com>
2024-08-07 16:00:47 +0900

    refactor: WebConfig 환경변수로 변경

--- src/main/java/com/gk/gourmet_note/common/config/WebConfig.java
+++ src/main/java/com/gk/gourmet_note/common/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.gk.gourmet_note.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
@@ -14,16 +15,15 @@ import java.io.IOException;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    /**
-     * 등록된 url 외의 요청은 vue 에서 받게 하기 위한 설정 ( vue router 사용 )
-     * -> static 폴더에 vue 프로젝트를 빌드할 때만 필요한 설정
-     * 같은 프로젝트로 배포하기 위해 필요한 설정임 ( Cloud Front 나 웹 호스팅을 통해 vue 프로젝트를 따로 배포 할 경우에는 필요 없음 )
-     * 하나의 인스턴스 서버에서 Nginx 등으로 배포할 때도 필요 없음
-     */
+    @Value("${upload.image.dir}")
+    private String dir;
+    @Value("${upload.image.path}")
+    private String path;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/upload/get/files/**")
-                .addResourceLocations("file:C:/study/upload/");
+        registry.addResourceHandler(path)
+                .addResourceLocations("file:" + dir);
 
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/static/")
